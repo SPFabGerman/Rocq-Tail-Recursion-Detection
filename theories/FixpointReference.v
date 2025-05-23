61f0bbd9b9132963d711079b2253c051a0652de1
Mario Frank <mario.frank@uni-potsdam.de>
2025-05-23 13:56:49 +0200

    Revamp the fixpoint reference documentations and added crossrefs in the file doc.

--- theories/FixpointReference.v
+++ theories/FixpointReference.v
@@ -1,41 +1,65 @@
+(** This file contains the basic definitions of [FixpointReference]
+    that models the relation between the calling and called function
+    that has a [FixpointReferenceKind].
+    Checking whether the function is a tailcall can be done using
+    [is_tailcall] and the information about a FixpointReference can
+    be represented as string using [string_of_FixpointReference]
+
+    Note: These are mainly produced in TailRecursionDetection.v and
+    used in Commands.v. *)
+
 From MetaRocq.Utils Require Import utils.
 From MetaRocq.Template Require Import All.
 
 Import IfNotations.
 
-(* This file contains the basic definitions of [FixpointReference].
-These are mainly produced in TailRecursionDetection.v and used in Commands.v. *)
 
+(** The FixpointReferenceKind differentiates between a [Tailcall] and
+    [NonTailcall] with the expected semantics.
+    Also, it may be a [StandaloneReference], i.e. a reference to a regular
+    function.
+    And finally, it can be an [UnsupportedTerm] which is mainly used to
+    indicate nested functions and fixpoints.*)
 Variant FixpointReferenceKind :=
-  | Tailcall : FixpointReferenceKind
-  | NonTailcall : FixpointReferenceKind
+  | Tailcall            : FixpointReferenceKind
+  | NonTailcall         : FixpointReferenceKind
   | StandaloneReference : FixpointReferenceKind
-  | UnsupportedTerm : FixpointReferenceKind. (* Used mainly to indicate nested functions and fixpoints. *)
+  | UnsupportedTerm     : FixpointReferenceKind.
 
-(** This is the main construct to record all important informationen
-of recursive calls.
-If [callterm] is [UnsupportedTerm], then [callee] is irrelevant and assumed to be [nAnon].
-(This should probably be solved with a Variant, not a Record, but for now it's fine.) *)
+
+(** This is the main construct to record all important informationen of
+    recursive calls which are
+
+    - the calling or referencing function [caller],
+    - the called/referenced function [callterm] and function name [callee].
+    - the definition in which the call is done and
+    - the [FixpointReferenceKind] [kind]
+
+    If [callterm] has kind [UnsupportedTerm], then [callee] is irrelevant and
+    assumed to be [nAnon].
+    (This should probably be solved with a Variant, not a Record, but for now
+    it's fine.) *)
 Record FixpointReference := mkFixpointReference {
-  caller : name; (* Which fixpoint calls? What contains the reference? *)
-  callee : name; (* Which fixpoint is being called? Which fixpoint is being referenced? *)
+  caller        : name;
+  callee        : name;
   in_definition : kername;
-  callterm : term;
-  kind : FixpointReferenceKind;
+  callterm      : term;
+  kind          : FixpointReferenceKind;
 }.
 
 
-(** Returns [true], iff the fixpoint reference is a [Tailcall].*)
+(** Returns [true], iff the fixpoint reference [fpr] is a [Tailcall].*)
 Definition is_tailcall (fpr : FixpointReference) : bool :=
   if fpr.(kind) is Tailcall then true else false.
 
 
+(** Creates a string representation of the FixpointReference [fpr]. *)
 Definition string_of_FixpointReference (fpr : FixpointReference) : string :=
   match fpr.(kind) with
-  | Tailcall => "Tail-Recursive Call of " ^ (string_of_name fpr.(callee))
-  | NonTailcall => "Non-Tail-Recursive Call of " ^ (string_of_name fpr.(callee))
+  | Tailcall            => "Tail-Recursive Call of " ^ (string_of_name fpr.(callee))
+  | NonTailcall         => "Non-Tail-Recursive Call of " ^ (string_of_name fpr.(callee))
   | StandaloneReference => "Standalone reference to " ^ (string_of_name fpr.(callee))
-  | UnsupportedTerm => "Unsupported term found" (* [callee] is ignored, if we have an unsupported term. *)
+  | UnsupportedTerm     => "Unsupported term found" 
   end ^ " in "
   ^ (string_of_name fpr.(caller)) ^ " ("
   ^ (string_of_kername fpr.(in_definition)) ^ "): "
