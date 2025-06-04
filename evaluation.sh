20d67e92814e33605084fe063dcbb7fded7d2976
SPFab <42518661+SPFabGerman@users.noreply.github.com>
2025-06-04 17:58:44 +0200

    Added dependency checking in OCaml and made evaluation script more generall

--- evaluation.sh
+++ evaluation.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Script to evaluate our algorithm, by comparing it against some extracted and compiled OCaml code.
+# Modules to check should be specified in ./examples/Evaluation.v
 # Run this script with `make evaluation`.
 # Output files can be found in ./examples/evaluation*
 
@@ -9,18 +10,17 @@ set -euo pipefail
 rm -rf ./examples/evaluation_extracted_code/ ./examples/evaluation_results_algorithm.txt ./examples/evaluation_results_compilation.txt
 
 # Run rocq to extract all OCaml programs and run our algorithm
-rocq compile -R theories TRchecker examples/Evaluation.v > ./examples/evaluation_results_algorithm.txt
+rocq compile -R theories TRchecker examples/Evaluation.v | sed 's/^.*(\(.*\)):.*$/\1/' | sort | uniq > ./examples/evaluation_results_algorithm.txt
 
-# Run a full compilation to native binary code with ocamlopt
-ocamlopt -o ./examples/evaluation_extracted_code/Nat.out -I ./examples/evaluation_extracted_code/ -g \
-    ./examples/evaluation_extracted_code/{Datatypes.mli,Datatypes.ml,Specif.mli,Specif.ml,Decimal.mli,Decimal.ml,Hexadecimal.mli,Hexadecimal.ml,Number.mli,Number.ml,Nat.mli,Nat.ml}
+# Run a full OCaml compilation to native binary code
+make -C ./examples/evaluation_extracted_code/ -f ../../Makefile_OCaml Program.out
 
-# Get all compilation symbols, that are functions and belong to the Nat group
-C=( $(objdump -t ./examples/evaluation_extracted_code/Nat.out | grep F | grep camlNat. | awk '{print $6}') )
+# Get all compilation symbols, that are functions and belong to the extracted ml files
+C=( $(objdump -t ./examples/evaluation_extracted_code/Program.out | grep F | grep "$(cd ./examples/evaluation_extracted_code/; ls *.ml | sed 's/^\(.*\).ml$/caml\1./')" | awk '{print $6}') )
 
 for c in "${C[@]}"; do
-    # echo "Symbol: $c"
     # Decompile each symbol and check for recursive calls
-    # Allow failures in grep, when no calls are found
-    objdump --disassemble=$c ./examples/evaluation_extracted_code/Nat.out | grep call | grep "$c" || true
-done > ./examples/evaluation_results_compilation.txt
+    if objdump --disassemble=$c ./examples/evaluation_extracted_code/Program.out | grep call | grep -q "$c"; then
+        echo "Found recursive call in: $c"
+    fi
+done | sort > ./examples/evaluation_results_compilation.txt
